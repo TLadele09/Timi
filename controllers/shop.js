cf3b2bbfb229431a4b2de9c7a8a3249cc3ce29b7
gig409 <garfield.gordon@brainchildeducation.com>
2020-05-01 11:36:43 +0100

    Made a change to shop controller for fetchAllProducts

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,8 +4,13 @@ const Sizes = require('../models/sizes')
 exports.getShopProducts = (req, res) => {
     const shop = new Shop()
     const result = shop.fetchProduct()
-    temp = shop.fetchProducts()
-    res.render('shop', {title: 'KBC SHOP', path: '/shop', data: result, temp: temp})
+    temp = shop.fetchAllProducts()
+    res.render('shop', {
+        title: 'KBC SHOP',
+        path: '/shop',
+        data: result,
+        temp: temp
+    })
 }
 exports.getShopProduct = (req, res) => {
     const shop = new Shop()
@@ -14,9 +19,15 @@ exports.getShopProduct = (req, res) => {
     const colour = shop.fetchProducts()
     const productId = req.params.product_id
     result = shop.fetchProduct(parseInt(productId))
-    console.log("This is", productSizes)  
+    console.log("This is", productSizes)
     // console.log("This is the product ID", productId)
-    res.render('detailpage', {title: 'KBC Product Detail', path: '/shop', temp: result, sizes: productSizes, colour: colour})
+    res.render('detailpage', {
+        title: 'KBC Product Detail',
+        path: '/shop',
+        temp: result,
+        sizes: productSizes,
+        colour: colour
+    })
 }
 exports.postShopProducts = (req, res) => {
     console.log(req.body.emailAddress, req.body.password)
@@ -32,7 +43,11 @@ exports.getJumpers = (req, res) => {
     result = shop.jumpers()
     temp = shop.fetchAllProducts()
     // console.log(temp)
-    res.render('jumpers', {title: 'KBC Jumpers', path: '/jumpers', temp: result})
+    res.render('jumpers', {
+        title: 'KBC Jumpers',
+        path: '/jumpers',
+        temp: result
+    })
 }
 exports.getJumper = (req, res) => {
     const shop = new Shop()
@@ -44,7 +59,13 @@ exports.getJumper = (req, res) => {
     console.log(result)
     // console.log("This is", mensSizes)
     // console.log("This is the product ID", productId)
-    res.render('detailpage', {title: 'KBC Jumper Detail', path: '/jumpers', temp: result, sizes: productSizes, colour: colour})
+    res.render('detailpage', {
+        title: 'KBC Jumper Detail',
+        path: '/jumpers',
+        temp: result,
+        sizes: productSizes,
+        colour: colour
+    })
 }
 exports.postJumpers = (req, res) => {
     // console.log(req.body.emailAddress, req.body.password)
@@ -58,7 +79,11 @@ exports.getTshirts = (req, res) => {
     result = shop.tshirts()
     temp = shop.fetchAllProducts()
     // console.log(temp)
-    res.render('tshirts', {title: 'KBC T-Shirts', path: '/tshirts', temp: result})
+    res.render('tshirts', {
+        title: 'KBC T-Shirts',
+        path: '/tshirts',
+        temp: result
+    })
 }
 exports.getTshirt = (req, res) => {
     const shop = new Shop()
@@ -70,7 +95,13 @@ exports.getTshirt = (req, res) => {
     console.log(result)
     // console.log("This is", mensSizes)
     // console.log("This is the product ID", productId)
-    res.render('detailpage', {title: 'KBC T-Shirt Detail', path: '/tshirts', temp: result, sizes: productSizes, colour: colour})
+    res.render('detailpage', {
+        title: 'KBC T-Shirt Detail',
+        path: '/tshirts',
+        temp: result,
+        sizes: productSizes,
+        colour: colour
+    })
 }
 exports.postTshirts = (req, res) => {
     // console.log(req.body.emailAddress, req.body.password)
@@ -89,14 +120,24 @@ exports.getHoodie = (req, res) => {
     console.log(result)
     // console.log("This is", mensSizes)
     // console.log("This is the product ID", productId)
-    res.render('detailpage', {title: 'KBC Hoodie Detail', path: '/hoodies', temp: result, sizes: productSizes, colour: colour})
+    res.render('detailpage', {
+        title: 'KBC Hoodie Detail',
+        path: '/hoodies',
+        temp: result,
+        sizes: productSizes,
+        colour: colour
+    })
 }
 exports.getHoodies = (req, res) => {
     const shop = new Shop()
     result = shop.hoodies()
     temp = shop.fetchAllProducts()
     // console.log(temp)
-    res.render('hoodies', {title: 'KBC Hoodies', path: '/hoodies', temp: result})
+    res.render('hoodies', {
+        title: 'KBC Hoodies',
+        path: '/hoodies',
+        temp: result
+    })
 }
 
 exports.postHoodies = (req, res) => {
@@ -106,5 +147,3 @@ exports.postHoodies = (req, res) => {
     temp = shop.fetchAllProducts()
     // console.log(temp)
 }
-
-
